7cdeab57500a624e5e76ea837d4ad9789bb0eca4
fahrudin25mhs-hub <fahrudin25.mhs@akb.ac.id>
2026-06-10 10:11:57 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,23 @@
 import { Sequelize } from "sequelize";
- const db = new Sequelize('web_lanjut', 'root', '', {
- host: "localhost",
- dialect: "mysql",
- "define": {
- "timestamps": false
- }
- });
- export default db;
+
+const db = new Sequelize(
+  'defaultdb',
+  'avnadmin',
+  'AVNS_ktuZR6H-KlqKKu7VSR-',
+  {
+    host: "mysql-1b97a8e2-akb-f455.h.aivencloud.com",
+    dialect: "mysql",
+    port: 28223,
+    define: {
+      timestamps: false
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+export default db;
