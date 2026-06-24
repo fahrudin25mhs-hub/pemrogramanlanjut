84a7e6894ebc4239f2d5be63b289566a622f714e
edkevind <edkevindlutfi@gmail.com>
2026-06-24 08:15:32 +0700

    test aqulize import

--- config/db.config.js
+++ config/db.config.js
@@ -1,26 +1,5 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
+console.log("sequelize imported");
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    define: {
-      timestamps: false,
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
-
-export default db;
+export default {};
