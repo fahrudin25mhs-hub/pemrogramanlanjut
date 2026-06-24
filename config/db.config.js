6eef7e92380f11cf955eecb66e387b67f7000c5d
edkevind <edkevindlutfi@gmail.com>
2026-06-24 09:07:18 +0700

    db config asli

--- config/db.config.js
+++ config/db.config.js
@@ -3,13 +3,27 @@ import { Sequelize } from "sequelize";
 
 console.log("MYSQL2 =", typeof mysql);
 
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_PASSWORD =", process.env.DB_PASSWORD ? "ADA" : "KOSONG");
+
 const db = new Sequelize(
-  "test",
-  "test",
-  "test",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
-    dialectModule: mysql
+    dialectModule: mysql,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
