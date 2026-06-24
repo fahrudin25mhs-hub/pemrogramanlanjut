cd61978d6796d10ef69a70263b7cbcb71062f6d9
edkevind <edkevindlutfi@gmail.com>
2026-06-24 08:40:16 +0700

    test db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,12 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
+console.log("sequelize imported");
+
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_PASSWORD =", process.env.DB_PASSWORD ? "ADA" : "KOSONG");
 
 const db = new Sequelize(
   process.env.DB_NAME,
@@ -9,17 +14,8 @@ const db = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "mysql",
-    define: {
-      timestamps: false,
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    port: process.env.DB_PORT,
+    dialect: "mysql"
   }
 );
 
