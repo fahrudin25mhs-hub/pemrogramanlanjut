ef85391d60552ee3a289ecf2bafde01b20954717
edkevind <edkevindlutfi@gmail.com>
2026-06-24 08:53:37 +0700

    aooo

--- config/db.config.js
+++ config/db.config.js
@@ -1,22 +1,5 @@
-import { Sequelize } from "sequelize";
+import mysql from "mysql2";
 
-console.log("sequelize imported");
+console.log("MYSQL2 =", typeof mysql);
 
-console.log("DB_NAME =", process.env.DB_NAME);
-console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_HOST =", process.env.DB_HOST);
-console.log("DB_PORT =", process.env.DB_PORT);
-console.log("DB_PASSWORD =", process.env.DB_PASSWORD ? "ADA" : "KOSONG");
-
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql"
-  }
-);
-
-export default db;
+export default {};
