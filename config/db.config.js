621237fd85c446758752fa457c6705ba1bd04737
edkevind <edkevindlutfi@gmail.com>
2026-06-24 08:34:09 +0700

    db config asli ke 2

--- config/db.config.js
+++ config/db.config.js
@@ -9,7 +9,7 @@ const db = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     define: {
       timestamps: false,
